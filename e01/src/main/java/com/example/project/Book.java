1b3f695755461c3f3152bc90a0dc31acaf66ead7
Ala <ala@Alas-MacBook-Pro.local>
2024-01-18 21:15:01 +0200

    1st exercise

--- e01/src/main/java/com/example/project/Book.java
+++ e01/src/main/java/com/example/project/Book.java
@@ -1,5 +1,46 @@
 package project;
 
 class Book {
+    private String title;
+    private String author;
+    private int numberOfPages;
 
+
+    /**
+     * The method for setting the book title
+     * - Length needs to be longer than 2
+     * @param theTitle Title of the book
+     */
+    public void setTitle(final String theTitle) {
+        this.title = theTitle;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+
+    public void setAuthor(final String theAuthor) {
+        this.author = theAuthor;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+
+    public void setPages(final int pageCount) {
+        this.numberOfPages = pageCount;
+    }
+
+    public int getPages() {
+        return this.numberOfPages;
+    }
+
+    public double calculateReadingTime() {
+        final int avgWordsPerPage = 250;
+        final double avgReadingSpeed = 200;
+        System.out.println((numberOfPages*avgWordsPerPage)/avgReadingSpeed);
+        return (numberOfPages*avgWordsPerPage)/avgReadingSpeed;
+    }
 }
--- e01/src/main/java/com/example/project/Main.java
+++ e01/src/main/java/com/example/project/Main.java
@@ -2,6 +2,6 @@ package project;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Book book1 = new Book();
     }
 }
