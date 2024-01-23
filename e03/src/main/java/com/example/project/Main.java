e7b360855d01ccd0be660a43ad4cca34be8a36c9
Ala <ala@Alas-MacBook-Pro.local>
2024-01-24 01:00:30 +0200

    exercise 3

--- e03/src/main/java/com/example/project/Main.java
+++ e03/src/main/java/com/example/project/Main.java
@@ -2,6 +2,24 @@ package project;
 
 public class Main {
     public static void main(String[] args) {
+
+        
+        Person person1 = new Person();
+        Person person2 = new Person("John", "Wick");
+        Person person3 = new Person("Hannah", "Montanah", 26);
+
+        System.out.println(person1.getFirstName());
+        System.out.println(person1.getLastName());
+        System.out.println(person1.getAge());
+
+        System.out.println(person2.getFirstName());
+        System.out.println(person2.getLastName());
+        System.out.println(person2.getAge());
+
+        System.out.println(person3.getFirstName());
+        System.out.println(person3.getLastName());
+        System.out.println(person3.getAge());
+       
         
     }
 }
--- e03/src/main/java/com/example/project/Person.java
+++ e03/src/main/java/com/example/project/Person.java
@@ -1,5 +1,45 @@
 package project;
 
 class Person {
+    //properties
+    private String name;
+    private String surName;
+    private int age;
 
+
+    //constructors
+    public Person() {
+        this.name = "";
+        this.surName = "";
+        this.age = 0;
+    }
+    public Person(final String n, String s) {
+        this.name = n;
+        this.surName = s;
+        this.age = 0;
+    }
+    public Person(final String n, String s, int a) {
+        this.name = n;
+        this.surName = s;
+        this.age = a;
+    }
+
+
+    //getters
+    public String getFirstName() {
+        return this.name;
+    }
+    public String getLastName() {
+        return this.surName;
+    }
+    public int getAge() {
+        return this.age;
+    }
+
+    //method
+    public void display() {
+        System.out.println(this.name);
+        System.out.println(this.surName);
+        System.out.println(this.age);
+    }
 }
