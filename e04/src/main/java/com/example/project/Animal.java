876819ea1d204e2e6b1adaa0a0b6e94ee47b8a3f
Ala <ala@Alas-MacBook-Pro.local>
2024-01-24 17:40:12 +0200

    Ex 4

--- e04/src/main/java/com/example/project/Animal.java
+++ e04/src/main/java/com/example/project/Animal.java
@@ -2,4 +2,40 @@ package project;
 
 class Animal {
 
+//properties
+private String name;
+private String type;
+private int age;
+
+
+//constructor 
+public Animal(String n, String t, int a) {
+    this.name = n;
+    this.type = t;
+    this.age = a;
 }
+//toString method
+public String toString() {
+return "Name: " + name + ", Type: " + type + ", Age: " + age;
+}
+
+//overriding "equals"
+public boolean equals(Animal n) {
+    return this.name == n.name && this.type == n.type && this.age == n.age;
+}
+
+//display
+public void display(Animal object) {
+    System.out.println(object.toString());
+}
+
+
+}
+
+
+/*
+ 
+
+4. Create multiple objects of the Animal class and test the `toString()` and `equals()` methods by comparing them and printing them to the console.
+6. Create a main method to test the toString, equals and display methods.
+ */
--- e04/src/main/java/com/example/project/Main.java
+++ e04/src/main/java/com/example/project/Main.java
@@ -2,6 +2,12 @@ package project;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Animal dog = new Animal("Buck", "Dog", 2);
+        Animal cat = new Animal("Paws", "Cat", 1);
+        Animal bird = new Animal("Pixel", "Canary", 3);
+
+        bird.toString();
+        dog.display(null);
+        cat.equals(bird);
     }
 }
